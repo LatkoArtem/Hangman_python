761a08ba0a33f32f95a8901b006ab8868eafa286
DESKTOP-4U7FGQI\Latko Artem <latko8973@gmail.com>
2023-11-05 19:34:16 +0200

    Add lab10

---
+++ Lab 10/task 1.py
@@ -0,0 +1,56 @@
+dirs = [
+    ( 'folder1',
+        [
+            'file1',
+            ( 'folder2', 
+                [
+                    'file2',
+                    'file3'
+                ] 
+            ),
+            ( 'folder3', 
+                [
+                    'file3', 
+                    'file4',
+                    ('folder4', ['file3'])
+                ] 
+            ),
+            'file5'
+        ]
+    ),
+    'file0'
+]
+
+
+def search(dir_tree, file_name, path=''):
+    res = []
+    for el in dir_tree:
+        if isinstance(el, str) and el == file_name:
+            res.append(f'{path}/{el}')
+        elif isinstance(el, tuple):
+            folder_name, sub_tree = el
+            new_path = path + '/' + folder_name
+            res.extend(search(sub_tree, file_name, new_path))        
+    return res
+        
+
+# ПЕРЕВІРКА
+
+print(search(dirs, 'file0'))
+print(search(dirs, 'file1'))
+print(search(dirs, 'file2'))
+print(search(dirs, 'file3'))
+print(search(dirs, 'file4'))
+print(search(dirs, 'file5'))
+print(search(dirs, 'file6'))
+print(search(dirs, 'folder1'))
+
+assert search(dirs, 'file0') == ['/file0'], 'Failed test for file0'
+assert search(dirs, 'file1') == ['/folder1/file1'], 'Failed test for file1'
+assert search(dirs, 'file2') == ['/folder1/folder2/file2'], 'Failed test for file2'
+assert search(dirs, 'file3') == ['/folder1/folder2/file3', '/folder1/folder3/file3', '/folder1/folder3/folder4/file3'], 'Failed test for file3'
+assert search(dirs, 'file4') == ['/folder1/folder3/file4'], 'Failed test for file4'
+assert search(dirs, 'file5') == ['/folder1/file5'], 'Failed test for file5'
+assert search(dirs, 'file6') == [], 'Failed test for file6'
+assert search(dirs, 'folder1') == [], 'Failed test for folder1'
+print('All tests good!')
